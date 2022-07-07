a94634dbcad0bfe206c9d97d5271a1460bd5e731
Rachel Bruce <rachelbruce922@gmail.com>
2022-07-07 17:29:21 -0500

    added images to gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/california.jpeg', description: 'Painting of Joshua Tree National Park.', likes: 0 },
+    { id: 2, path: 'images/florida.jpeg', description: 'Painting of a nature preserve in Florida', likes: 0 },
+    { id: 3, path: 'images/maine.jpeg', description: 'Painting of Stonington, Maine.', likes: 0 },
+    { id: 4, path: 'images/montana.jpeg', description: 'Painting of Medicine Rocks State Park in Montana.', likes: 0 }
 ];
 
 module.exports = galleryItems;
