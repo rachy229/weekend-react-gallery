42f9bcacf9c19636c653e0ea1409af08133aacf7
Rachel Bruce <rachelbruce922@gmail.com>
2022-07-08 11:23:52 -0500

    working on put request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,15 +4,28 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import GalleryItem from '../GalleryItem/GalleryItem';
 import GalleryList from '../GalleryList/GalleryList';
+import { response } from 'express';
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  const fetchGallery = () => {
+    axios.get('/gallery')
+    .then((response) => {
+      console.log('response in fetchGallery', response);
+      console.log('response.data in fetchGallery', response.data);
+      setGalleryList(response.data);
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/maine.jpeg"/>
       </div>
     );
 }
