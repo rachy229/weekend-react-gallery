566e9235eaca11f62f46094bc6993a3395dad900
Rachel Bruce <rachelbruce922@gmail.com>
2022-07-09 13:33:58 -0500

    made the image description appear on click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 function GalleryItem({item}) {
+
+    const [swap, setSwap] = useState(false)
+
+const toggleImage = () => {
+        // console.log('click')
+        setSwap(!swap);
+        // console.log(swap);
+}
+
+const showDescription = () => {
+        if(swap === true) {
+            return <p class="galleryBox" onClick={toggleImage}>{item.description}</p>
+        }else{
+            return <img class="galleryBox galleryDescription" onClick={toggleImage} src={item.path}></img>
+        }
+    }
+
+    
+
+
     return(
+
         <div>
-            <div key={item.id}>
+        {/* { this.showDescription() } */}
+            {/* <div key={item.id}>
                 {item.description}
-            </div>
-            
-            <img src={item.path}></img>
+            </div> */}
+            {/* <img onClick={toggleImage} src={item.path}></img> */}
+            {showDescription()}
         </div>
     );
 }
